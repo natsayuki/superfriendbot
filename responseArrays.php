54cca8345ab79a38c09d6730f65f790cc3651a8d
the42ndturtle <the42ndturtle@gmail.com>
2018-05-24 22:10:18 -0400

    init commit

---
+++ responseArrays.php
@@ -0,0 +1,48 @@
+<?php
+  $arrPhrasesBasicResponses = array(
+  "Wood ducks are highly dangerous",
+  "Wood ducks can climb trees",
+  "Download Wood duck simulator",
+  "Wood duck is spooky aminal",
+  "There have been an average 13k recorded deaths from wood ducks every year",
+  "Looking a wood duck in the eyes agrivates it to a point of no return",
+  "Wood ducks get their name because they are ducks made of wood",
+  "Wood ducks only eat human flesh",
+  "Wood ducks only drink human blood",
+  "Wood ducks have poison tipped claws",
+  "If you spot a wood duck, kill it",
+  "Wood ducks cannot fly, only flap their wings and levitate"
+  );
+
+  $arrPhrasesHangOn = array(
+  "Sure thing, hang on a sec...",
+  "Let me get that for you, hang on a sec...",
+  "Let me get that for you, hang tight...",
+  "Coming right up, just a sec...",
+  "Gotta put that together, hold on...",
+  "Let me look that up, hold on...",
+  "Got it, lemme get that info. Hold on..."
+  );
+
+  $arrPhrasesWelcome = array(
+  "I would never do you a bamboozle",
+  "How much wood could a wood duck duck if a duck could duck wood?",
+  "Yes"
+  );
+
+  $arrPhrasesAfterSorry = array(
+  "No biggie!",
+  "My feelings aren't hurt. Actually, I don't have feelings, so it's a win-win",
+  "I accept your apology (if that's what you'd call it)"
+  );
+
+  $arrPhrasesPositiveFeedback = array(
+  "Awww :)",
+  "*blush*",
+  "I love you too, I think",
+  "Is 'love' an emotion for humans only?",
+  "For a human, you're pretty attractive",
+  "Wanna go out sometime?"
+  );
+
+?>
